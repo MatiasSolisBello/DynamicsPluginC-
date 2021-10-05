cbecd78ef240d258b473c6c4510ffdc75808e056
MatiasSolisBello <matias_solis11@hotmail.com>
2021-10-05 18:12:42 -0300

    Change State (Before Testing)

--- TestTrainee_Plugin/TestTrainee.cs
+++ TestTrainee_Plugin/TestTrainee.cs
@@ -13,45 +13,95 @@ namespace TestTrainee_Plugin
     {
         public void Execute(IServiceProvider serviceProvider)
         {
-            //ITracingService tracingService =
-            //(ITracingService)serviceProvider.GetService(typeof(ITracingService));
-
             IPluginExecutionContext context = (IPluginExecutionContext)
                 serviceProvider.GetService(typeof(IPluginExecutionContext));
 
-            Entity entity = (Entity)context.InputParameters["Target"];
+            //Inicializar Trace
+            ITracingService tracer = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
 
-            if (entity.Contains("ms_departamento"))
+            //
+            IOrganizationServiceFactory serviceFactory =
+                   (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+
+            //
+            IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
+
+           
+
+            //valida si evento es de creacion -> sino no se ejecuta
+            if (!context.MessageName.Equals("create", StringComparison.InvariantCultureIgnoreCase))
             {
-                IOrganizationServiceFactory serviceFactory = 
-                    (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
-                IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
+                tracer.Trace("context.MessageName distinto de create");
+                return;
+            }
 
-                string fetchXml =
-                @"<fetch version=""1.0"" output-format=""xml - platform"" mapping=""logical"" distinct=""false""><entity name = ""ms_reserva"" >
-                    < attribute name = ""ms_reservaid"" />
-                    < attribute name = ""ms_reserva"" />
-                    < attribute name = ""createdon"" />
-                    < order attribute = ""ms_reserva"" descending = ""false"" />
-                    < filter type = ""and"" >
-                        < condition attribute = ""statecode"" operator= ""eq"" value = ""0"" />
-                        < condition attribute = ""ms_departamento"" operator= ""eq"" value = ""af361822-8111-ec11-b6e7-002248376565"" />
-                    </ filter >
-                    </ entity >
-                    </ fetch > 
-                ";
-                fetchXml = string.Format(fetchXml, entity.Id);
-                var qe = new FetchExpression(fetchXml);
-                var result = service.RetrieveMultiple(qe);
-                foreach (var e in result.Entities)
+            //evitar caida de target -> condicional/entity
+            if (context.InputParameters.Contains("Target") && context.InputParameters["Target"] is Entity)
+            {
+                Entity entity = (Entity)context.InputParameters["Target"];
+
+                //LogicalName: ms_reserva. StringComparison: Omitir mayusculas, ñ, tildes
+                if (!entity.LogicalName.Equals("ms_reserva", StringComparison.InvariantCultureIgnoreCase))
+                {   
+                    return; 
+              
+                }
+                
+                try
                 {
-                    Entity updatedDepart = new Entity(e.LogicalName);
-                    updatedDepart.Id = e.Id;
-                    updatedDepart["ms_departamento"] = entity["ms_departamento"];
-                    service.Update(updatedDepart);
+                    if (entity.Contains("ms_departamento"))
+                    {
+                        tracer.Trace("TestTrainee_Plugin: Contiene ms_departamento");
+                        tracer.Trace("TestTrainee_Plugin: EntityLogicalName: " + entity.GetAttributeValue<EntityReference>("ms_departamento"));
+
+                        //obtener atributos del entity
+                        var departId = entity.GetAttributeValue<EntityReference>("ms_departamento").Id;
+                        tracer.Trace("TestTrainee_Plugin: EntityLogicalName: "+ entity.GetAttributeValue<EntityReference>("ms_departamento").LogicalName);
+
+                        string fetchXml =
+                        @"<fetch version=""1.0"" output-format=""xml - platform"" mapping=""logical"" distinct=""false""><entity name = ""ms_reserva"" >
+                        < attribute name = ""ms_reservaid"" />
+                        < attribute name = ""ms_reserva"" />
+                        < attribute name = ""createdon"" />
+                        < order attribute = ""ms_reserva"" descending = ""false"" />
+                        < filter type = ""and"" >
+                            < condition attribute = ""statecode"" operator= ""eq"" value = ""0"" />
+                            < condition attribute = ""ms_departamento"" operator= ""eq"" value = ""{0}"" />
+                        </ filter >
+                        </ entity >
+                        </ fetch > 
+                        ";
+                        fetchXml = string.Format(fetchXml, departId);
+                        var qe = new FetchExpression(fetchXml);
+                        var result = service.RetrieveMultiple(qe);
+                        //foreach (var e in result.Entities)
+                        //{
+                        //    Entity updatedDepart = new Entity(e.LogicalName);
+                        //    updatedDepart.Id = e.Id;
+                        //    updatedDepart["ms_departamento"] = entity["ms_departamento"];
+                        //    service.Update(updatedDepart);
+                        //}
+
+                        if (result.Entities.Count()>0)
+                        {
+                            Entity updatedDepart = new Entity(entity.GetAttributeValue<EntityReference>("ms_departamento").LogicalName);
+                            updatedDepart.Id = entity.GetAttributeValue<EntityReference>("ms_departamento").Id;
+
+                            updatedDepart["ms_estado"] = new OptionSetValue(100000003);
+                            service.Update(updatedDepart);
+                        }
+                    }
+
+                    
+                }
+                
+                catch (Exception e)
+                {
+                    //saca el error -> else -> (e.String)
+                    throw new InvalidPluginExecutionException(e.Message);
                 }
             }
-            
+
         }
     }
 }
